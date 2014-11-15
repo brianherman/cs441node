d7870b201926a79857acb0251095afd3a1390de4
Brian Herman <brianherman@gmail.com>
2014-11-14 20:20:52 -0600

    added untracked files that were supposed to be added

--- bherma3/routes/document.js
+++ bherma3/routes/document.js
@@ -2,43 +2,40 @@ var express = require('express');
 var router = express.Router();
 var redis = require("redis"),
     client = redis.createClient(6379,'54.173.24.13',{});
-
+var tick = 0;
 /* GET users listing. */
 router.get('/push', function(req, res) {
   if(!req.param('key') || !req.param('value') || !req.param('tick'))
       res.send("FAIL");
-  var p = client.get(req.param('key')+'$tick', function(err, tick) {
-    var tic = parseInt(req.param('tick'));
-        if(err){console.log('Key Does not exist');}
-        if(req.param('tick') < tic){
-           client.get(req.param('key'), function(err, doc) {
-            res.send({
-                'time': tic,
-                'data': doc
-                });
-            return;
-           });
-        }
-    });
-
-  client.set(req.param('key'),req.param('value'), redis.print);
-  tic++;
   client.set(req.param('key')+'$tick',req.param('tic'), redis.print);
   res.send({
-    'time': tic
+    'TYPE': "PUSH"
+    'DATA': "SUCCESS"
   });
 });
+router.get('/time', function(req, res) {
+  client.get(req.param('key')+'$'+'tick', function(err, time){
+      if(parseInt(req.param('tick')) > parseInt(time)){
+        client.set(req.param('key')+'$'+'tick');
+        res.send({
+          'TIME': time
+          'OP', 'PUSH'
+        });
 
+      }else{
+        res.send({
+          'TIME': req.param('tick'),
+          'OP', 'PULL'
+        });
+      }
+    });
+});
 router.get('/pull', function(req, res) {
-  if(!req.param('tick')){
-      res.send({'message':"FAIL"});
-    }else{
-  client.get(req.param('key'), function(err, doc) {
-     client.get(req.param('key')+'$tick', function(err, tick) {
+  client.get(req.param('key'), function(err,doc){
         if(err){console.log('Key Does not exist');}
         res.send({
-            'data':doc.toString(),
-            'time': tick
+            'TYPE': "GET"
+            'DATA':doc.toString(),
         });
      });
   });
